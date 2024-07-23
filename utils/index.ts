227db05f169966717d66e16ebc533eecadc32769
Jaya krishna <Krishna@Krishnas-MacBook-Air.local>
2024-07-23 15:15:49 +0530

    updated comments

--- utils/index.ts
+++ utils/index.ts
@@ -1,41 +1,41 @@
 import { CarProps, FilterProps } from "@types";
 
 export const calculateCarRent = (city_mpg: number, year: number) => {
-  const basePricePerDay = 50; // Base rental price per day in dollars
-  const mileageFactor = 0.1; // Additional rate per mile driven
-  const ageFactor = 0.05; // Additional rate per year of vehicle age
+  const basePricePerDay = 50; 
+  const mileageFactor = 0.1; 
+  const ageFactor = 0.05; 
 
-  // Calculate additional rate based on mileage and age
+
   const mileageRate = city_mpg * mileageFactor;
   const ageRate = (new Date().getFullYear() - year) * ageFactor;
 
-  // Calculate total rental rate per day
+
   const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
 
   return rentalRatePerDay.toFixed(0);
 };
 
 export const updateSearchParams = (type: string, value: string) => {
-  // Get the current URL search params
+
   const searchParams = new URLSearchParams(window.location.search);
 
-  // Set the specified search parameter to the given value
+  
   searchParams.set(type, value);
 
-  // Set the specified search parameter to the given value
+
   const newPathname = `${window.location.pathname}?${searchParams.toString()}`;
 
   return newPathname;
 };
 
 export const deleteSearchParams = (type: string) => {
-  // Set the specified search parameter to the given value
+
   const newSearchParams = new URLSearchParams(window.location.search);
 
-  // Delete the specified search parameter
+
   newSearchParams.delete(type.toLocaleLowerCase());
 
-  // Construct the updated URL pathname with the deleted search parameter
+
   const newPathname = `${window.location.pathname}?${newSearchParams.toString()}`;
 
   return newPathname;
@@ -44,13 +44,13 @@ export const deleteSearchParams = (type: string) => {
 export async function fetchCars(filters: FilterProps) {
   const { manufacturer, year, model, limit, fuel } = filters;
 
-  // Set the required headers for the API request
+ 
   const headers: HeadersInit = {
     "X-RapidAPI-Key":  "8b75d4ec27mshd51c0ff503d9be3p1fd356jsnf4de7df0db1d",
     "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
   };
 
-  // Set the required headers for the API request
+
   const response = await fetch(
     `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
     {
@@ -58,7 +58,7 @@ export async function fetchCars(filters: FilterProps) {
     }
   );
 
-  // Parse the response as JSON
+ 
   const result = await response.json();
 
   return result;
@@ -73,7 +73,7 @@ export const generateCarImageUrl = (car: CarProps, angle?: string) => {
   url.searchParams.append('modelFamily', model.split(" ")[0]);
   url.searchParams.append('zoomType', 'fullscreen');
   url.searchParams.append('modelYear', `${year}`);
-  // url.searchParams.append('zoomLevel', zoomLevel);
+  
   url.searchParams.append('angle', `${angle}`);
 
   return `${url}`;
